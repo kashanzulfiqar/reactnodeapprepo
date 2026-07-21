d553181cb0b580011f288ef3c8ac4d84e2b62ce4
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-07-21 11:02:36 +0500

    Change AMI ID in staging main.tf
    Updated AMI ID for the staging environment.

--- infra/environments/staging/main.tf
+++ infra/environments/staging/main.tf
@@ -16,7 +16,7 @@ module "staging_infra" {
   
   environment   = "staging"
   instance_type = "t3.micro"       # Small instance for staging
-  ami_id        = "ami-0c7217cdde317cfec" # Replace with valid Ubuntu/Amazon Linux AMI in us-east-1
+  ami_id        = "ami-0ac1f955d6e62f3f1" # Replace with valid Ubuntu/Amazon Linux AMI in us-east-1
 }
 
 output "staging_ec2_ip" {
