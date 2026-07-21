9632d590789869365dc564fbd1617cec0eabf636
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-07-21 15:44:44 +0500

    Update main.tf

--- infra/environments/staging/main.tf
+++ infra/environments/staging/main.tf
@@ -23,7 +23,7 @@ module "staging_infra" {
   
   environment   = "staging"
   instance_type = "t3.micro"       # Small instance for staging
-  ami_id        = "ami-0ac1f955d6e62f3f1" # Replace with valid Ubuntu/Amazon Linux AMI in us-east-1
+  ami_id        = "ami-0b6d9d3d33ba97d99" # Replace with valid Ubuntu/Amazon Linux AMI in us-east-1
 }
 
 output "staging_ec2_ip" {
