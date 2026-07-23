b48e9b8ced983aa428d35320b5908cd0c64ee77a
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-07-23 13:05:12 +0500

    Update main.tf

--- infra/modules/app_stack/main.tf
+++ infra/modules/app_stack/main.tf
@@ -115,6 +115,7 @@ resource "aws_instance" "backend" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.backend_sg.id]
+  key_name               = var.key_name
 
   tags = {
     Name        = "backend-${var.environment}"
