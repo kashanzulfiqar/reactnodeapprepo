1ef4c9738cff12303331b2343e5598d5ba6f27b5
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-07-27 11:33:45 +0500

    Update outputs.tf

--- infra/modules/app_stack/outputs.tf
+++ infra/modules/app_stack/outputs.tf
@@ -9,3 +9,7 @@ output "cloudfront_distribution_id" {
 output "ec2_public_ip" {
   value = aws_eip.backend_eip.public_ip
 }
+output "cloudfront_domain_name" {
+  value       = aws_cloudfront_distribution.cdn.domain_name
+  description = "Domain name of the CloudFront distribution"
+}
