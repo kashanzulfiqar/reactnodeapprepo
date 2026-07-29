1d4877b0658f54fd9f8dbfc3d3187ffc67624b60
kashanzulfiqar <56619464+kashanzulfiqar@users.noreply.github.com>
2026-07-29 16:14:51 +0500

    Update main.tf

--- infra/modules/app_stack/main.tf
+++ infra/modules/app_stack/main.tf
@@ -129,7 +129,12 @@ resource "aws_security_group" "backend_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
